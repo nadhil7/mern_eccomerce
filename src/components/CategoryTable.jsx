951f2a6e3f58d6a537d23ff309d43ad092a91761
nadhilkn <nadhunadhil33429@gmail.com>
2025-08-13 12:39:37 +0530

    1st

--- src/components/CategoryTable.jsx
+++ src/components/CategoryTable.jsx
@@ -4,24 +4,41 @@ import Instance from '../Axios'
 
 function CategoryTable() {
     const [category, setcategory] = useState([])
+    const [show, setshow] = useState("")
+    const [name, setname] = useState("")
+    const [discription, setdiscription] = useState()
 
     const data = async () => {
-        const response = await Instance.get("/category/",)
-        console.log(response);
+        try {
+            const response = await Instance.get("/category/",)
+            setcategory(response.data)
+        }
+        catch (err) {
+            console.log(err);
 
-        setcategory(response.data)
+        }
     }
     useEffect(() => {
         data();
     }, []);
+
     const editCategory = async (id) => {
-        console.log(id);
-        const response = await Instance.patch(`/admin/useredit/${id}`)
-        data();
-    }
-    const deleteCategory = async () => {
         try {
-            
+            const formData = new FormData()
+            formData.append("name", name)
+            formData.append("discription", discription)
+
+        }
+        catch (err) {
+            console.log(err);
+
+        }
+    }
+    const deleteCategory = async (id) => {
+        console.log(id);
+
+        try {
+
         }
         catch (err) {
 
@@ -29,6 +46,41 @@ function CategoryTable() {
     }
     return (
         <>
+            {show && <div className='max-w-sm mx-auto fixed left-3/6 bg-gray-400 p-10 top-30 -translate-x-3/6'>
+                <div class="mb-5">
+                    <label for="email"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Category Name</label>
+                    <input type="email" id="email"
+                        class="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                          focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
+                           dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                            dark:focus:border-blue-500 dark:shadow-xs-light"
+                        placeholder="Category Name" required />
+                </div>
+                <div class="mb-5">
+                    <label for="repeat-password"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Discription</label>
+                    <input type="password" id="repeat-password" class="shadow-xs bg-gray-50 border
+                         border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500
+                          block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+                           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
+                        required />
+                </div>
+                <div className='flex justify-center items-center gap-1'>
+                    <button type="submit item"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                     focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-700
+                      dark:hover:bg-gray-600 dark:focus:ring-blue-800" onClick={() => { setshow(false) }}>Cancel</button>
+                    <button type="submit item"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                     focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600
+                      dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                </div>
+            </div >}
+            <div className='flex justify-end p-4'>
+                <button onClick={() => { setshow(true); editCategory(); seteditid(null) }} className='p-4 bg-green-400 rounded-2xl font-bold'>Add +</button>
+            </div>
+
             <div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10 py-6 ">
                 <table className="table-fixed ml-56 w-10/12 text-center">
                     <thead className='text-center'>
@@ -39,20 +91,22 @@ function CategoryTable() {
                             <th className="w-1/4  p-4 text-gray-600 font-bold uppercase">Action</th>
                         </tr>
                     </thead>
+
                     <tbody className="bg-white ">
                         {category.map((i, index) =>
                             <tr key={index}>
                                 <td className='w-2/4 p-4 font-semibold  text-xl'>{index + 1}</td>
                                 <td className="w-2/4 p-4 font-semibold text-xl">{i.name}</td>
                                 <td className="w-2/4 p-4 font-semibold text-xl">{i.discription}</td>
-                                <td className='w-1/4 '>
-                                    <button className='w-20 h-10 rounded text-black font-bold  bg-green-600' onClick={() => { editCategory(i._id) }}>Edit</button>
-                                    <button className='w-20 h-10 rounded text-black font-bold  bg-green-600' onClick={() => { deleteCategory(i._id) }}>deleteproduct</button>
+                                <td className='w-full flex flex-col p-1 gap-2 items-center justify-center '>
+                                    <button className='w-20 h-8 rounded text-black font-bold  bg-green-600' onClick={() => { saveproduct(i._id) }}>Edit</button>
+                                    <button className='w-20 h-8 rounded text-black font-bold  bg-red-600' onClick={() => { deleteCategory(i._id) }}>delete</button>
                                 </td>
                             </tr>
                         )}
                     </tbody>
                 </table>
+
             </div>
         </>
     )
